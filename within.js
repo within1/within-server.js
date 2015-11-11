e42b677cfcde8194132a4d811e4836731195d80b
Joel Solymosi <joel@custlabs.com>
2015-11-11 06:49:37 -0800

    Basic server structure; session cookies, and MSSQL connection

--- within.js
+++ within.js
@@ -1,13 +1,29 @@
 var express = require("express");
-var expressSession = require("express-session");
+var session = require("express-session");
 var compression = require('compression')
 var http = require('http');
 var models = require("./models");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // start web server
 var app = express();
-app.use(compression({ threshold: 512}));
+app.use(compression({ threshold: 512 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// apply sessions
+app.use(cookieParser())
+app.use(session({
+  secret: 'keyboard cat',
+  store: new SequelizeStore({
+    db: models.sequelize
+  }),
+  resave: true,
+  saveUninitialized : true,
+  proxy: true // if you do SSL outside of node.
+}))
 
 app.use('/assets', express.static(__dirname + '/assets'));
 
@@ -18,10 +34,11 @@ app.get("/", function(req, res) {
 app.use('/assets', express.static(__dirname + '/assets'));
 
 models.sequelize.sync().then(function() {
+	/*
 	models.sequelize.query("SELECT * FROM users", { type: models.sequelize.QueryTypes.SELECT}).then(function(users) {
 		console.log("query success:");
 		console.log(JSON.stringify(users,0,4));
-	});
+	}); */
 	var server = http.createServer(app);
 	if (process.env.PORT == undefined) {
 		process.env.PORT = 1337;
