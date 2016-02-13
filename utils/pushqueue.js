c3bd59a3125e327b50df865217241df22ef46e05
Joel Solymosi <joel@custlabs.com>
2016-02-13 01:25:43 -0800

    daemon fix

--- utils/pushqueue.js
+++ utils/pushqueue.js
@@ -1,8 +1,13 @@
 var notif = require("../lib/notifications.js")
 var models = require("../models");
 
+var daemon = require("../lib/daemon.js");
+
+/*
 models.Users.findOne({where : { ID : 44}})
 .then(function(u) {
 	return notif.SendPushNotification(u, 0, "tester tests", null, 0);
 })
 
+*/
+console.log(daemon.notifRefill);
