d00ad5dc65f17c1fc42a3d2939e2833c03e5d8e6
Joel Solymosi <joel@custlabs.com>
2016-02-16 19:12:48 -0800

    APN feedback; GetMessageThread

--- test/api/GetMessageThread.js
+++ test/api/GetMessageThread.js
@@ -1,17 +1,19 @@
 // GetMessageThread API tests
 
-var test = require("./tests.js");
+var test = require("../tests.js");
+var persona = require("../persona.js")();
 
 test("GetMessageThread", [
 {
     msg: "Listing messages",
     url: "GetMessageThread",
     postdata : {
-    	"UserID" : "1",
-    	"UserToken" : "Dbr/k5trWmO3XRTk3AWfX90E9jwpoh59w/EaiU9df/OkFa6bxluaKsQmBtKDNDHbBpplmFe2Zo06m6TOpxxDc0mhb1DzDq0EzXjBFsfQRVTewDXwdZZ5mxNdEp4HEdrIlx43DPPRh+5uQzOzP8bob7ckkNvE7yB9HbeZVS5I1BhjHA3/8Ac2Qf0+sjkHb8mKk/bSO1NammUS3jYXzGPcYv7AtzG7h11zX7yDAylMvmg00thD6v961ofF+ukq1RskAHfwYwBZ1Wuz8ofII/JqUranUrT9omVQ",
-    	"SenderID" : "2",
+    	"UserID" : persona["UserID"],
+    	"UserToken" : persona["UserToken"],
+    	"SenderID" : persona["anotherUserID"],
     	"MessageCount" : 5
     },
     expect : {}
 
 }]);
+
