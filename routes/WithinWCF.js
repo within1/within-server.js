229aa72ff8ebc9ee790d527049118f48c33f0777
Joel Solymosi <joel@custlabs.com>
2016-02-23 16:17:20 -0800

    all APIs to be handled by nodejs server

--- routes/WithinWCF.js
+++ routes/WithinWCF.js
@@ -23,16 +23,7 @@ function routeProxy(funcname, postdata, cb) {
 	var baseurl = (process.env.NODE_ENV == "development") ? ("http://dev.within.guru/") : ("http://app.within.guru/");
 	if (process.env.NODE_ENV == "local")
 		baseurl = "http://within.local/";
-	var url = "";
-	if (process.env.NODE_ENV == "live") {
-		url = baseurl+"WCF/Service1.svc/"+funcname;
-		var apifuncs = ["GetAllUsers", "GetMatchesForUser", "UploadPicture"];
-		if (apifuncs.indexOf(funcname) != -1) {
-			url = baseurl+"api/"+funcname;
-		}
-	} else {
-		url = baseurl+"api/"+funcname;
-	}
+	var url = baseurl+"api/"+funcname;
 	console.log(url);
 	request({uri: url, method: "POST", json : postdata }, function(error, res, body) {
 		if (error != null)
