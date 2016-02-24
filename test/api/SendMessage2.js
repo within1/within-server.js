b9cd0c9f3aac2e734d084462d44c779f09712f98
Joel Solymosi <joel@custlabs.com>
2016-02-24 00:00:11 -0800

    docs update

--- test/api/SendMessage2.js
+++ test/api/SendMessage2.js
@@ -10,7 +10,7 @@ test("SendMessage", [
     postdata : {
 
 	    "Type": "1",
-	    "Message": "Hello world! asd123",
+	    "Message": "Hello world! qwd qwdq wqwd qwd qwdqwd",
 	    "ReceiverID": "4073",
     	"UserID" : "4078",
     	"UserToken" : "Dbr/k5trWmO3XRTk3AWfX90E9jwpoh59w/EaiU9df/OkFa6bxluaKsQmBtKDNDHbBpplmFe2Zo06m6TOpxxDc3iaHQaFLsi1zXjBFsfQRVTewDXwdZZ5mxNdEp4HEdrIQY6VRqDvBzltACUdl2CB+gr1grGpDN+UmOnCUh9wD+BcROYXx5SmyTNtFYi+oKU7gjPLI9dWeoJWLVLUmAr6I5O8cIbXa7vWMNTTfGRgAsQBMVgU8j40sofF+ukq1RskAHfwYwBZ1Wuz8ofII/JqUranUrT9omVQ",
